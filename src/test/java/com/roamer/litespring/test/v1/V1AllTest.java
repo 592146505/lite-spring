ee1a0294a987dd875bd90c4d966ae7498526be06
roamer_home <592146505@qq.com>
2018-06-21 19:59:12 +0800

    第2周代码(3)：使用junit  Suite套件进行集中式测试

---
+++ src/test/java/com/roamer/litespring/test/v1/V1AllTest.java
@@ -0,0 +1,19 @@
+package com.roamer.litespring.test.v1;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * v1版集中测试类
+ *
+ * @author roamer
+ * @version V1.0
+ * @date 2018/6/21 19:55
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        BeanFactoryTest.class,
+        ApplicationContextTest.class
+})
+public class V1AllTest {
+}
